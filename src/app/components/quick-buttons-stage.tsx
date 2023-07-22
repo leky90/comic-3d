f7a60ce1d2677791074d8ec54540c51fb893e2d7
Ky Le <ldky90@gmail.com>
2023-07-22 13:01:05 +0700

    update intro

--- src/app/components/quick-buttons-stage.tsx
+++ src/app/components/quick-buttons-stage.tsx
@@ -1,9 +1,13 @@
 import { ROUTES } from '../constants/route.constant';
 import { useRoute } from 'wouter';
 import { useHashLocation } from '../hooks/use-hash-location';
+import { useState } from 'react';
+
+let defaultAccept = false;
 
 export function QuickButtonsStage() {
   const [, setLocation] = useHashLocation();
+  const [accepted, setAccepted] = useState(defaultAccept);
   const [matchedComics, params] = useRoute(ROUTES['comics-page']);
   const currentPage = params === null ? 1 : Number(params.page);
 
@@ -25,34 +29,50 @@ export function QuickButtonsStage() {
     );
   }
 
+  function accept() {
+    defaultAccept = true;
+    setAccepted(defaultAccept);
+  }
+
   return (
     <>
-      <button
-        className={`rotate-w white-button ${matchedComics ? 'show' : 'hide'}`}
-        onClick={up}
-      >
+      <div className={`box-center ${accepted ? 'hide' : 'show'}`}>
+        <h2 style={{ color: 'yellow' }}>Hướng dẫn sử dụng:</h2>
+
+        <ul style={{ lineHeight: 1.5 }}>
+          <li>Bấm phím hoặc nhấp (Esc) để thoát</li>
+          <li>
+            <mark>Double click</mark> vào truyện để xem chi tiết
+          </li>
+          <li className="hide-mb">Bấm phím (W) hoặc (&uarr;) để đi lên</li>
+          <li className="hide-mb">Bấm phím (S) hoặc (&darr;) để đi xuống</li>
+          <li className="hide-mb">Bấm phím (A) hoặc (&larr;) để xoay trái</li>
+          <li className="hide-mb">Bấm phím (D) hoặc (&rarr;) để xoay phải</li>
+          <li className="hide show-mb">Nhấp vào nút (&uarr;) để đi lên</li>
+          <li className="hide show-mb">Nhấp vào nút (&darr;) để đi xuống</li>
+        </ul>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <button
+            className="white-button"
+            style={{ fontSize: 14 }}
+            onClick={accept}
+          >
+            Đã hiểu
+          </button>
+        </div>
+      </div>
+      <button className={`rotate-w white-button `} onClick={up}>
         &uarr; <br />
         (W)
       </button>
-      <button
-        className={`rotate-s white-button ${matchedComics ? 'show' : 'hide'}`}
-        onClick={down}
-      >
+      <button className={`rotate-s white-button `} onClick={down}>
         (S) <br />
         &darr;
       </button>
-      <button
-        className={`rotate-a white-button ${matchedComics ? 'show' : 'hide'}`}
-      >
-        &larr; (A)
-      </button>
-      <button
-        className={`rotate-d white-button ${matchedComics ? 'show' : 'hide'}`}
-      >
-        (D) &rarr;
-      </button>
+      <button className={`rotate-a white-button `}>&larr; (A)</button>
+      <button className={`rotate-d white-button `}>(D) &rarr;</button>
       <button onClick={back} className="back-button">
-        Quay lại (Esc)
+        (Esc)
       </button>
     </>
   );
